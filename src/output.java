0c20cd4e512cfb14ee26b5fe7f88aa0b1c2b410e
tristantapson8 <tapsonte@mcmaster.ca>
2018-04-27 21:10:56 -0400

    Update output.java

--- src/output.java
+++ src/output.java
@@ -11,20 +11,19 @@ public class output {
 	
 	public static void openFile(String s1, String s2, String d1, BigDecimal err){
 		
-	try{
-        	String path = "results.txt";
-        	File file = new File(path);
-        	FileWriter fileWriter = new FileWriter(file,true);
-		BufferedWriter bufferFileWriter  = new BufferedWriter(fileWriter);
+		try{
+        		String path = "results.txt";
+        		File file = new File(path);
+        		FileWriter fileWriter = new FileWriter(file,true);
+			BufferedWriter bufferFileWriter  = new BufferedWriter(fileWriter);
 
-        	fileWriter.append( "Name: " + s1 + " // Device: " +  s2 + " // Completion Time: " + d1 + " // Error Percentage: " + err + "% ");
-        	bufferFileWriter.close();
+        		fileWriter.append( "Name: " + s1 + " // Device: " +  s2 + " // Completion Time: " + d1 + " // Error Percentage: " + err + "% ");
+        		bufferFileWriter.close();
 
-        //System.out.println("User Registration Completed");
+       			//System.out.println("User Registration Completed");
 
-    }catch(Exception ex)
-    {
-        System.out.println(ex);
-    }
-}
+    		}catch(Exception ex){
+        		System.out.println(ex);
+    		}
+	}
 }
